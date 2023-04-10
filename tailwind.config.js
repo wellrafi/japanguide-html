a29ccb7ede9ab47772d6f6a68a6fc3e77a9eec8f
wellrafi <wellrafiuniverse@gmail.com>
2023-04-10 11:32:39 +0700

    chnge contnet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './public/**/*.{html,js}'
+    './**/*.{html,js}',
+    './*.{html,js}'
   ],
   theme: {
     minHeight: {
