1cce62ce3ef4f05fd3aaa7ca12c2a6912a510b47
wellrafi <wellrafiuniverse@gmail.com>
2023-04-10 11:17:56 +0700

    changes

---
+++ public/scripts/tailwind-config.js
@@ -0,0 +1,33 @@
+tailwind.config = {
+    theme: {
+      minHeight: {
+        lg: '700px',
+        xl: '860px',
+      },
+      fontFamily: {
+        'alte': ['AlteHaasGroteskRegular'],
+        'altebold': ['AlteHaasGroteskBold']
+      },
+      extend: {
+        margin: {
+          'header': "margin-top: 139px"
+        },
+        height: {
+          'px-34' : "2.125rem"
+        },
+        width: {
+          'px-530': "33.125rem"
+        },
+        colors: {
+          clifford: '#dadada',
+          primary: '#F34646',
+          darkprimary: '#C23333',
+        },
+        container: {
+            padding: "15px",
+            center: true,
+            '2xl': '1350px'
+        }
+      }
+    }
+}
